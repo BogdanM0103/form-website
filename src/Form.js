d05ef9fddd81bc517707bd690658908541fbd41e
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-05-05 15:00:01 +0300

    some changes

--- src/Form.js
+++ src/Form.js
@@ -5,7 +5,7 @@ const Form = () => {
     // Define state variables to store form data
     const [formData, setFormData] = useState({
         firstName: '',
-        lastName: '',
+        secondName: '',
         email: '',
         phone: '',
         file: null
@@ -39,15 +39,15 @@ const Form = () => {
 
     return ( 
         <div className="container">
-            <form onSubmit={ handleSubmit }>
+            <form onSubmit={ handleSubmit } action="" method="POST" encType="multipart/form-data">
                 <div className="first-row">
                     <div className="first-name">
                         <label htmlFor="firstName">First Name:</label>
                         <input type="text" id="firstName" name="firstName" value={ formData.firstName } onChange={ handleChange } />
                     </div>
                     <div className="second-name">
-                        <label htmlFor="lastName">Second Name:</label>
-                        <input type="text" id="lastName" name="lastName" value={ formData.secondName } onChange={ handleChange } />
+                        <label htmlFor="secondName">Second Name:</label>
+                        <input type="text" id="secondName" name="secondName" value={ formData.secondName } onChange={ handleChange } />
                     </div>
                 </div>
                 <div className="second-row">
@@ -61,10 +61,10 @@ const Form = () => {
                     </div>
                 </div>
                 <div className="third-row">
-                    <div className="file-upload">
-                        <label htmlFor="file">Upload File:</label>
-                        <input type="file" id="file" name="file" value={ formData.file } onChange={ handleChange } />
-                    </div>
+                    {/* <div className="file-upload"> */}
+                        {/* <label htmlFor="file">Upload File:</label> */}
+                        {/* <input type="file" id="file" name="file" value={ formData.file } onChange={ handleChange } /> */}
+                    {/* </div> */}
                     <div className="submit-button">
                         <button type="submit">Submit</button>
                     </div>
